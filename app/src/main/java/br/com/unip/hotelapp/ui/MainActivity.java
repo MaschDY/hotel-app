e32efba2328211fa4229f597b047e833d887c41c
maschdy <leonardocovelodapaz@gmail.com>
2021-11-12 22:38:48 -0300

    first commit

---
+++ app/src/main/java/br/com/unip/hotelapp/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package br.com.unip.hotelapp.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import br.com.unip.hotelapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
